03ed20aeb1ce4771a30bc26a11bdb50ca62b8cef
motojate <motojate@naver.com>
2023-11-13 23:58:43 +0900

    * 상수 설정

---
+++ src/constants/datas.ts
@@ -0,0 +1,14 @@
+export const NUMBER_OF_MENU_ITEMS: { label: string; value: number }[] = [
+  {
+    value: 10,
+    label: '10개',
+  },
+  {
+    value: 20,
+    label: '10개',
+  },
+  {
+    value: 30,
+    label: '10개',
+  },
+]
