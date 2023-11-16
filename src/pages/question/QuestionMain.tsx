e5c19a2a65cd7d58cc0c5c9db0e3dde7d6b45b39
motojate <motojate@naver.com>
2023-11-16 23:41:02 +0900

    * 필요없는 주석 제거

--- src/pages/question/QuestionMain.tsx
+++ src/pages/question/QuestionMain.tsx
@@ -1,24 +1,15 @@
 import { ThemeProvider } from '@emotion/react'
-import { KeyboardArrowLeft, KeyboardArrowRight } from '@mui/icons-material'
 import {
   createTheme,
   CssBaseline,
   Container,
-  FormControl,
-  Button,
   Box,
   AppBar,
   Toolbar,
   Typography,
   MobileStepper,
 } from '@mui/material'
-import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import QuestionCountDropdown from 'src/components/atoms/dropdowns/QuestionCountDropdown'
-import SplashScreenMainImageField from 'src/components/molecule/fields/SplashScreenMainImageField'
-import { NUMBER_OF_MENU_ITEMS } from 'src/constants/datas'
-import { setQuestionCount } from 'src/modules/question/questionSlice'
-import { useAppDispatch, useAppSelector } from 'src/store'
+import { useAppSelector } from 'src/store'
 
 const theme = createTheme({
   palette: {
@@ -33,7 +24,6 @@ const theme = createTheme({
     MuiAppBar: {
       styleOverrides: {
         root: {
-          // 헤더 및 푸터 크기 조정
           minHeight: '60px',
         },
       },
@@ -54,7 +44,7 @@ const QuestionMain = () => {
       </AppBar>
       <Box
         sx={{
-          height: 'calc(100vh - 120px)', // 전체 높이에서 헤더와 푸터 높이를 제외
+          height: 'calc(100vh - 120px)',
           backgroundColor: theme.palette.primary.main,
         }}
       >
@@ -66,10 +56,9 @@ const QuestionMain = () => {
             p: 2,
             borderRadius: 1,
             boxShadow: 1,
-            height: '100%', // 컨테이너 높이를 최대로 설정
+            height: '100%',
           }}
         >
-          {/* 여기에 컨텐츠 추가 */}
           <MobileStepper
             variant="progress"
             steps={state.count}
