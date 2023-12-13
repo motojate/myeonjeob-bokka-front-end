d2d36741baeec2e82c110ee6880a788a3bd92905
motojate <motojate@naver.com>
2023-12-13 23:18:39 +0900

    * 문구 변경

--- src/components/molecules/notices/snack-bars/SnackBar.tsx
+++ src/components/molecules/notices/snack-bars/SnackBar.tsx
@@ -3,7 +3,7 @@ import { Snackbar, Alert } from '@mui/material'
 const SnackBar = () => {
   return (
     <Snackbar open={true} message={'hi'} autoHideDuration={1000}>
-      <Alert severity="error">This is an error message!</Alert>
+      <Alert severity="error">{''}</Alert>
     </Snackbar>
   )
 }
