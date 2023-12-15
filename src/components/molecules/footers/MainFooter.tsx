9bddad78c72613a12fc41e06bfd76d560378926d
motojate <motojate@naver.com>
2023-12-15 17:14:09 +0900

    * 필요없는 주석 제거

--- src/components/molecules/footers/MainFooter.tsx
+++ src/components/molecules/footers/MainFooter.tsx
@@ -1,12 +1,9 @@
-// Footer 컴포넌트
 import React from 'react'
-import { Box, Typography, Container } from '@mui/material'
+import { Box, Typography } from '@mui/material'
 
 const Footer: React.FC = () => {
   return (
     <Box component="footer" sx={{ py: 1, backgroundColor: '#9A7DFF' }}>
-      {' '}
-      {/* 여기서 py는 y축 패딩을 의미합니다. */}
       <Typography variant="overline" align="center" sx={{ lineHeight: '1.2' }}>
         © 2023 Your App Name
       </Typography>
